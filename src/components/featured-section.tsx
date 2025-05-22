325f33128962576ed756f8ed71971318fbb3499f
mledezma <marcoledezmacordero09@gmail.com>
2025-05-21 22:26:00 -0600

    Small content change.

--- src/components/featured-section.tsx
+++ src/components/featured-section.tsx
@@ -35,7 +35,7 @@ export function FeaturedSection() {
             <div className="max-w-[540px]">
               <h2 className="text-2xl md:text-4xl font-semibold mb-1">Somos SIPSA</h2>
               <h3 className="mb-8 text-xl md:text-3xl font-semibold">SOLUCIONES INTEGRALES PRIME S.A.</h3>
-              <p className="text-lg mb-4">Somos una empresa de transportes locales e internacionales que se enorgullece de su compromiso y experiencia trabajando con grandes empresas como lo son Gualapack, Molinos de Guadalupe, Laboratorios Griffith, en el área de alimentos.</p>
+              <p className="text-lg mb-4">Somos una empresa de transportes locales e internacionales que se enorgullece de su compromiso y experiencia trabajando con grandes empresas en la industria alimentaria e industrial.</p>
               <p className="text-lg mb-4">Contamos con diversos camiones, lo que nos permite ofrecer soluciones de transporte flexibles y confiables, ofreciéndole nuestro servicio desde los almacenes fiscales o para realizar alguna entrega o recolección a nivel local.</p>
               <p className="text-lg mb-4">Nuestra trayectoria con clientes de renombre respalda nuestra dedicación a la excelencia en el servicio. Nos encantaría platicar de cómo podemos satisfacer las necesidades de transporte de la empresa y contribuir al éxito de sus operaciones.</p>
             </div>
